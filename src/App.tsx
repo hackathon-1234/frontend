44093bbd958587817203a373141f30ac057ed425
Samur <107584285+Samur000@users.noreply.github.com>
2023-05-20 14:13:07 +0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,43 @@ import React from 'react';
 import './App.css';
 import Counter from "./components/Counter";
 import Posts from "./components/Posts";
-import {useAppDispatch, useAppSelector} from "./hooks/useRedux";
-import {checkAuth} from "./store/slices/auth/ActionCreators";
-import {Typography} from "antd";
-import {Route, Routes} from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "./hooks/useRedux";
+import { checkAuth } from "./store/slices/auth/ActionCreators";
+import { Typography } from "antd";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/loginPage";
 import RegistrationPage from "./pages/registrationPage";
+import { Link } from 'react-router-dom';
 
 function App() {
-    const {isLoading} = useAppSelector(state => state.auth)
-    const dispatch = useAppDispatch();
+  const { isLoading } = useAppSelector(state => state.auth)
+  const dispatch = useAppDispatch();
 
-    React.useEffect(() => {
-        if (localStorage.getItem('token')) {
-            dispatch(checkAuth())
-        }
-    }, [])
-
-    if (isLoading) {
-        return <Typography>Загрузка</Typography>
+  React.useEffect(() => {
+    if (localStorage.getItem('token')) {
+      dispatch(checkAuth())
     }
+  }, [])
+
+  if (isLoading) {
+    return <Typography>Загрузка</Typography>
+  }
 
   return (
     <div className="App">
-        <Counter />
-        <Posts />
+      <Counter />
+      <Posts />
 
-        <Routes>
-            <Route path={"/"} element={<h1>Foo</h1>} />
-            <Route path={"/login"} element={<LoginPage />} />
-            <Route path={"/registration"} element={<RegistrationPage />} />
-        </Routes>
+      <nav>
+        <Link to='/login'>Login</Link>
+        <Link to='/registration'>Registration</Link>
+      </nav>
+
+      <Routes>
+        <Route path={"/"} element={<h1>Foo</h1>} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/registration"} element={<RegistrationPage />} />
+      </Routes>
     </div>
   );
 }
